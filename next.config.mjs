ac478c584c9aba544ad7e8988b7e5b9a00799994
apexlawnaniket <abhavsar@apexlawncompany.com>
2024-11-29 14:13:05 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 
 const isProd = process.env.NODE_ENV = 'producation';
 const nextConfig = {
-  basePath = isProd ? '/nextjs-test' : ''
+  basePath : isProd ? '/nextjs-test' : '',
 };
 
 export default nextConfig;
